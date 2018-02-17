458157f171dba6e3aca144a6a8b679d2514e8abf
David Mouser <david.mouser@irexchange.com>
2018-02-17 14:37:30 +1100

    Some more refactoring

--- neb-slack-bot.py
+++ neb-slack-bot.py
@@ -1,28 +1,34 @@
 import json
+import logging
+import psutil
 import re
 import subprocess
-import psutil
+import sys
 import time
 from hurry.filesize import si
 from hurry.filesize import size
 from slackclient import SlackClient
 
-SLACK_BOT_USER_ID = "pi-bot1"
+logging.basicConfig(filename='/var/log/neb/slack-bot.log',
+                    format='%(asctime)s: [%(levelname)s] %(message)s',
+                    level=logging.INFO)
+
+SLACK_BOT_USER_NAME = "pi-bot1"
 SLACK_BOT_API_TOKEN = "xoxb-316184120996-JdkNoxj2EXusbapl8CQ0IKov"
 
-intervals = (
-    ('weeks', 604800),
-    ('days', 86400),
-    ('hours', 3600),
-    ('minutes', 60),
-    ('seconds', 1),
-)
 
+def friendly_time(seconds, granularity=3):
+    time_intervals = (
+        ('weeks', 604800),
+        ('days', 86400),
+        ('hours', 3600),
+        ('minutes', 60),
+        ('seconds', 1),
+    )
 
-def friendly_time(seconds, granularity=2):
     result = []
 
-    for name, count in intervals:
+    for name, count in time_intervals:
         value = seconds // count
         if value:
             seconds -= value * count
@@ -40,6 +46,26 @@ def find_process_by_name(name):
     return ls
 
 
+def get_processes_running_now():
+    return [(p.pid, p.info['name']) for p in psutil.process_iter(attrs=['name', 'status']) if
+            p.info['status'] == psutil.STATUS_RUNNING]
+
+
+def get_processes_sorted_by_memory():
+    return sorted(psutil.process_iter(attrs=['name', 'memory_percent']), key=lambda p: p.info['memory_percent'])
+
+
+def get_processes_sorted_by_cpu():
+    return sorted(psutil.process_iter(attrs=['name', 'cpu_times', 'cpu_percent']), key=lambda p: sum(p.info['cpu_times'][:2]))
+
+
+def fetch_slack_user_id(username):
+    user_list = slack_client.api_call("users.list")
+    for user in user_list.get('members'):
+        if user.get('name') == username:
+            return user.get('id')
+
+
 def send_slack_response(response_message):
     slack_client.api_call("chat.postMessage", channel=message['channel'], text=response_message, as_user=True)
 
@@ -54,32 +80,30 @@ def sanitize_slack_message_text():
         strip()
 
 
+# When starting from reboot we need to delay as we might not have a network connection yet!
+delay_startup = float(sys.argv[1]) if len(sys.argv) > 1 else 0
+logging.info("Starting up in %d secs" % delay_startup)
+time.sleep(delay_startup)
+
 slack_client = SlackClient(SLACK_BOT_API_TOKEN)
+slack_user_id = fetch_slack_user_id(SLACK_BOT_USER_NAME)
 
-# Fetch your Bot's User ID
-user_list = slack_client.api_call("users.list")
-for user in user_list.get('members'):
-    if user.get('name') == SLACK_BOT_USER_ID:
-        slack_user_id = user.get('id')
-        break
-
-# Start connection
+# Initiate a slack connection and wait for messages
 if slack_client.rtm_connect():
-    print "Successfully connected to Slack. Waiting for messages..."
+    logging.info('Successfully connected to Slack. Waiting for messages...')
 
     while True:
         for message in slack_client.rtm_read():
             if 'text' in message and slack_message_tagged_user_marker(slack_user_id) in message['text']:
 
-                # print "Message received: %s" % json.dumps(message, indent=2)
-                print "Message received: %s" % message['text']
+                logging.info("Message received: %s" % message['text'])
                 message_text = sanitize_slack_message_text()
 
                 if re.match(r'.*(staking).*', message_text, re.IGNORECASE):
                     neb_staking_info = json.loads(subprocess.check_output("/home/pi/nebliod getstakinginfo | jq .", shell=True).strip())
                     slack_response = "Yeah, I'm collecting all your nebbles!\n" \
                                      "Your weight is *%s*. I estimate you'll get your next stake in about *%s*." \
-                                     % (neb_staking_info['weight'], friendly_time(neb_staking_info['expectedtime'], 3)) \
+                                     % (neb_staking_info['weight'], friendly_time(neb_staking_info['expectedtime'])) \
                         if neb_staking_info['staking'] == True else "No, not right now."
 
                     send_slack_response(slack_response)
@@ -91,30 +115,27 @@ if slack_client.rtm_connect():
                     send_slack_response(slack_response)
 
                 elif re.match(r'.*(most ram|most memory).*', message_text, re.IGNORECASE):
-                    top_processes_mem = reversed([(p.pid, p.info) for p in sorted(psutil.process_iter(attrs=['name', 'memory_percent']),
-                                                                                  key=lambda p: p.info['memory_percent'])][-5:])
+                    top_processes_mem = reversed([(p.pid, p.info) for p in get_processes_sorted_by_memory()][-5:])
                     slack_response = "These are my *top 5* processes using the most memory:\n%s" % "\n".join(
-                        "  %s. *%s*,  %s%% (pid: %s)" % (idx + 1, p[1]['name'], round(p[1]['memory_percent'], 2), p[0]) for idx, p in
-                        enumerate(
-                            top_processes_mem)) if top_processes_mem != None else "Well this is embarassing... I couldn't work that out!"
+                        "  %s. *%s*,  %s%% (pid: %s)" % (idx + 1, p[1]['name'], round(p[1]['memory_percent'], 2), p[0]) for idx, p in enumerate(top_processes_mem)) \
+                        if top_processes_mem is not None else "Well this is embarrassing... I couldn't work that out!"
 
                     send_slack_response(slack_response)
 
                 elif re.match(r'.*(most cpu).*', message_text, re.IGNORECASE):
-                    top_processes_cpu = reversed([(p.pid, p.info, sum(p.info['cpu_times'])) for p in
-                                                  sorted(psutil.process_iter(attrs=['name', 'cpu_times', 'cpu_percent']),
-                                                         key=lambda p: sum(p.info['cpu_times'][:2]))][-5:])
-                    slack_response = "These are my *top 5* processes using the most CPU:\n%s" % "\n".join(
-                        "  %s. *%s*,  %s (pid: %s)" % (idx + 1, p[1]['name'], friendly_time(p[2]), p[0]) for idx, p in enumerate(
-                            top_processes_cpu)) if top_processes_cpu != None else "Well this is embarassing... I couldn't work that out!"
+                    top_processes_cpu = reversed([(p.pid, p.info, sum(p.info['cpu_times'])) for p in get_processes_sorted_by_cpu()][-5:])
+                    slack_response = "These are my *top 5* processes using the most CPU:\n%s" % "\n"\
+                        .join("  %s. *%s*,  %s (pid: %s)" % (idx + 1, p[1]['name'],
+                                                             friendly_time(p[2]), p[0]) for idx, p in enumerate(top_processes_cpu)) \
+                        if top_processes_cpu is not None else "Well this is embarrassing... I couldn't work that out!"
 
                     send_slack_response(slack_response)
 
                 elif re.match(r'.*(active).*', message_text, re.IGNORECASE):
-                    active_processes = [(p.pid, p.info['name']) for p in psutil.process_iter(attrs=['name', 'status']) if
-                                        p.info['status'] == psutil.STATUS_RUNNING]
-                    slack_response = "I have these *active* processes running:\n%s" % "\n".join("  %s (pid: %s)" % (p[1], p[0]) for p in
-                                                                                                active_processes) if active_processes != None else "There are no processes running at the moment."
+                    active_processes = get_processes_running_now()
+                    slack_response = "I have these *active* processes running:\n%s" % "\n"\
+                        .join("  %s. *%s* (pid: %s)" % (idx + 1, p[1], p[0]) for idx, p in enumerate(active_processes)) \
+                        if active_processes is not None else "There are no processes running at the moment."
 
                     send_slack_response(slack_response)
 
@@ -145,7 +166,7 @@ if slack_client.rtm_connect():
                                                                      size(disk.free, system=si),
                                                                      size(disk.used, system=si))
 
-                    send_slack_response("My disk space is *%s%%* free.\n%s" % (disk_pct, disk_detail))
+                    send_slack_response("My disk is at *%s%%* of capacity.\n%s" % (disk_pct, disk_detail))
 
                 elif re.match(r'.*(hello|hey|hi).*', message_text, re.IGNORECASE):
                     send_slack_response("Hellllo! And how are you?")
